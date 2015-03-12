c5ff2132fc1ec081b91fabaff7d75f3a5821dde7
crcapretta <capretx@gmail.com>
2015-03-12 23:19:46 +0100

    Prima Commit

--- src/complexnumber/complexNumber.java
+++ src/complexnumber/complexNumber.java
@@ -1,89 +1,184 @@
-package complexnumber;
+package complexNumber;
 
-public class complexNumber {
+public class ComplexNumber {
 
-    private double modulus;
-    private double argument;
-    private double re;
-    private double im;
+	// TODO Auto-generated method stub
+		     private double modulus;
+		     private double argument;
+		     public static double initModulus;
+		     public static double initArgument;
+		     private double re;
+		     private double im;
+		     public static double initRe;
+		     public static double initIm;
+		     private StringFormat format;
+		  
+		    enum StringFormat{
+		    	RECTANGULAR,
+		    	POLAR
+		    }
+		     
+		    static public void setInitRectangular( double re,double im){
+		        initRe=re;
+		        initIm=im;
+		     
+		    }    
+		    static public void setInitPolar(double modulus, double argument){
+		        initModulus=modulus;
+		        initArgument=argument;
+		    }    
+		    public void complexNumber( ){
+		    	 this.re = initRe;
+				 this.im = initIm;
+				
+			}
+			public String formatComplexNumber() {
+			   String r = this.getRe() + "+(" + this.getIm() + ")i";
+			   return r;
+			}
+
+			public double getRe() {
+				re=Math.sqrt(modulus*modulus)-(im*im);
+		        return re;
+			}
+
+			public void setRe(double re){
+				if(re<=modulus){ 
+				   this.re = re; 
+		 	   } 
+		 	   else{ 
+				   throw new IllegalArgumentException("The value of a must be different from 0."); 
+			   } 
+
+			}
+
+			public double getIm() {
+			   im=Math.sqrt(modulus*modulus)-(re*re);
+			   return im;
+			}
+
+			public void setIm(double im) {
+				if(re<=modulus){ 
+			    this.im=im; 
+			   } 
+		 	   else{ 
+		 		   throw new IllegalArgumentException("The value of a must be different from 0."); 
+		 	   } 
+
+		    }
+			
+			public double getModulus(){
+				modulus=Math.sqrt((re*re)+(im*im));
+				return modulus;
+			}
+			
+			public void setModulus(double modulus){
+				if(modulus>=0){ 
+				   this.modulus=modulus; 
+		 	    } 
+		 		else{ 
+		 			throw new IllegalArgumentException("The value of a must be different from 0."); 
+		 		} 
+
+			}
+			
+			public double getArgument(){
+			    argument=Math.atan((im/re)+Math.PI);
+				return argument;
+			}
+			
+			public void setArgument(double argument){
+				this.argument=argument;
+			}
 		    
-    public complexNumber(double re,double  im) {
-       this.re = re;
-	   this.im = im;
-	   }
-		    
-	public complexNumber() {
-	   this.re = 0;
-       this.im = 0;
-       }
-		    
-    public String formatComplexNumber() {
-	   String r = this.getRe() + "+(" + this.getIm() + ")i";
-	   return r;
-	}
+		     static public ComplexNumber add(ComplexNumber operand1, ComplexNumber operand2){
+		        ComplexNumber somma= new ComplexNumber();
+		        somma.setInitRectangular(operand1.getRe()+operand2.getRe(),operand1.getIm()+operand2.getIm());
+		        return somma;
+		          
+		      }
+		     static public ComplexNumber sub(ComplexNumber operand1, ComplexNumber operand2){
+		    	ComplexNumber sottrazione=new ComplexNumber();
+		        sottrazione.setInitRectangular(operand1.getRe()-operand2.getRe(),operand1.getIm()-operand2.getIm());
+		        return sottrazione;
+		      }
+		      static public ComplexNumber multiply(ComplexNumber operand1, ComplexNumber operand2){
+		        ComplexNumber moltiplicazione= new ComplexNumber();
+		        moltiplicazione.setInitPolar(operand1.getArgument()+operand2.getArgument(),operand1.getModulus()*operand2.getModulus());
+		        return moltiplicazione;
+		        
+		    }
+		    static public ComplexNumber divide(ComplexNumber operand1, ComplexNumber operand2){
+		        ComplexNumber divisione = new ComplexNumber();
+		        divisione.setInitPolar(operand1.getArgument()-operand2.getArgument(),operand1.getModulus()/operand2.getModulus());
+		        return divisione;
+		        
+		        
+		    }
+		     static public ComplexNumber setConjugate(ComplexNumber operand) {
+				ComplexNumber congiunzione = new ComplexNumber();
+				congiunzione.setInitRectangular(operand.getRe(),-operand.getIm());
+				return congiunzione;
+		         
+		     } 
+		     
+		      public ComplexNumber add(ComplexNumber operand){
+			        ComplexNumber somma= new ComplexNumber();
+			        somma.setInitRectangular(this.getRe()+operand.getRe(),this.getIm()+operand.getIm());
+			        return somma;
+			  }
+		      
+		       public ComplexNumber sub(ComplexNumber operand){
+			    	ComplexNumber sottrazione=new ComplexNumber();
+			        sottrazione.setInitRectangular(this.getRe()-operand.getRe(),this.getIm()-operand.getIm());
+			        return sottrazione;
+			    
+		      }
+		       
+		       public ComplexNumber multiply(ComplexNumber operand){
+			        ComplexNumber moltiplicazione= new ComplexNumber();
+			        moltiplicazione.setInitPolar(this.getArgument()+operand.getArgument(),this.getModulus()*operand.getModulus());
+			        return moltiplicazione;
+			   }
+		       
+		       public ComplexNumber divide(ComplexNumber operand){
+			        ComplexNumber divisione = new ComplexNumber();
+			        divisione.setInitPolar(this.getArgument()-operand.getArgument(),this.getModulus()/operand.getModulus());
+			        return divisione;
+		       }
+		       
+		        public ComplexNumber setConjugate() {
+					ComplexNumber congiunzione = new ComplexNumber();
+					congiunzione.setInitRectangular(this.getRe(),-this.getIm());
+					return congiunzione;
+		       }
+		     public String toString() {
+		    	 return toString(this.format);
+		    	 
+		     }
+		     
+		     public String toString(StringFormat format) {
+		    	 String r = new String(); 
+		    	 switch(format) { 
+		    	     case RECTANGULAR: 
+		    	     r = this.getRe() + "+(" + this.getIm() + ")i"; 
+		    	     break; 
+		    	  	 case POLAR: 
+		    	  	 r = this.getModulus() + "*exp(i*" + this.getArgument() +")"; 
+		    	  	 break; 
+		    	 } 
+		   		 return r; 
+		 	}
+	    
+		     @Override public boolean equals(Object o) { 
+		         boolean equal = false; 
+		         if(o instanceof ComplexNumber) { 
+		      	    ComplexNumber pointer = (ComplexNumber)o; 
+		      		if(this.re == pointer.getRe() && this.im == pointer.getIm()) 
+		      		equal = true; 
+		      	} 
+		     		return equal;
+		     }
 
-	public double getRe() {
-       return re;
-	}
-
-	public void setRe(double re) {
-	   if(re<=modulus){
-	   this.re = re;
-	   }
-	   else{
-		   throw new IllegalArgumentException("The value of a must be different from 0.");
-	   }
-	}
-	public double getIm() {
-	   return im;
-	}
-
-	public void setIm(double im) {
-	   if(im<=modulus){
-	      this.im = im;
-        }
-	   else{
-		   throw new IllegalArgumentException("The value of a must be different from 0.");
-	   }
-	}
-	public double getModulus(){
-		return modulus;
-	}
 	
-	public void setModulus(double modulus){
-		if(modulus>=0){
-		   this.modulus=modulus;
-	    }
-		else{
-			throw new IllegalArgumentException("The value of a must be different from 0.");
-		}
-	}
-	public double getArgument(){
-		return argument;
-	}
-	
-	public void setArgument(double argument){
-		this.argument=argument;
-	}
-	
-	public double calcolaModulus(){
-	   modulus=Math.sqrt((re*re)+(im*im));
-	   return modulus;    	
-    }
-	
-    public double calcolaArgument(){
-	   argument=Math.atan((im/re)+Math.PI);
-	   return argument;
-    }
-    
-    public double caloclaRe() {
-    	re=Math.sqrt((modulus*modulus)-(im*im));
-		return re;
-	}
-    
-    public double calcolaIm(){
-    	im=Math.sqrt((modulus*modulus)-(re*re));
-    	return im;
-    }
-
 }
--- src/ese102/ese102.java
+++ src/ese102/ese102.java
@@ -1,125 +1,132 @@
 package ese102;
 import java.io.*;
 
-import complexnumber.*;
-public class ese102 {
+import complexNumber.ComplexNumber;
+public class Ese102 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader tastiera = new BufferedReader(input);
-		int a=0;
-        complexNumber cn=new complexNumber();
-        boolean valoreValido=false;
-        
-   
-        while(!valoreValido){
-           System.out.println("digitare un numero minore o uguale a 10 se vuoi inserire le coordinate polari o uno superiore se vuoi inserire le coordinate cartesiane\n");
-           try {
-              String numeroLetto=tastiera.readLine();
-              a=Integer.valueOf(numeroLetto).intValue();
-              valoreValido=true;
-           }
-           catch(NumberFormatException e) {
-        	   System.out.println("\nnumero non valido");
-           }
-        }
-        valoreValido=false;
-		if(a>10){
-			while(!valoreValido){
-			   System.out.println("\ninserisci il valore reale");
-			   try{
-				   String numeroLetto=tastiera.readLine();
-				   cn.setRe(Double.valueOf(numeroLetto).doubleValue());
-				   valoreValido=true;
-			   }
-			   catch(NumberFormatException e) {
-				   System.out.println("\nnumero non valido");
-				   
-			   }
-			}
-			valoreValido=false;
-			while(!valoreValido){
-			   System.out.println("\ninserisci il valore immaginario");
-			   try{
-				   String numeroLetto=tastiera.readLine();
-				   cn.setIm(Double.valueOf(numeroLetto).doubleValue());
-				   valoreValido=true;
-			   }
-			   catch(NumberFormatException e) {
-				   System.out.println("\nnumero non valido");
-			   }
-        	System.out.println("\nil modulo è "+cn.calcolaModulus());
-        	System.out.println("\nl'argomento è "+cn.calcolaArgument());
-			}
-        }
-		else{
-			   valoreValido=false;
-				while(!valoreValido){
-				   System.out.println("\ninserisci il modulo");
-				   try{
-					   String numeroLetto=tastiera.readLine();
-					   cn.setModulus(Double.valueOf(numeroLetto).doubleValue());
-					   valoreValido=true;
-				   }
-				   catch(NumberFormatException e) {
-					   System.out.println("\nnumero non corretto");
-				   }
-				   catch(IllegalArgumentException e){
-					   System.out.println("\nil valore impostato non è ammesso");
-				   }
-				}
-				   valoreValido=false;
-				   while(!valoreValido){
-				      System.out.println("\ndigitare un numero minore o uguale a 10 per inserire il numero reale o uno superiore per inserire il numero immaginario");
-				      try{
-					      String numeroLetto=tastiera.readLine();
-					      a=Integer.valueOf(numeroLetto).intValue();
-					      valoreValido=true;
-				      }
-				      catch(NumberFormatException e){
-				    	  System.out.println("\nnumero non valido");
-				      }
-				}
-				if(a==1){
-					valoreValido=false;
+
+				InputStreamReader input = new InputStreamReader(System.in);
+		        BufferedReader tastiera = new BufferedReader(input);
+				int a=0;
+		        ComplexNumber coordinate=new ComplexNumber();
+		        boolean valoreValido=false;
+		        
+		        ComplexNumber.setInitRectangular(45, 7);
+		        ComplexNumber.setInitPolar(30, 5);
+		       
+		        
+		   
+		        while(!valoreValido){
+		           System.out.println("digitare un numero minore o uguale a 10 se vuoi inserire le coordinate polari o uno superiore se vuoi inserire le coordinate cartesiane\n");
+		           try {
+		              String numeroLetto=tastiera.readLine();
+		              a=Integer.valueOf(numeroLetto).intValue();
+		              valoreValido=true;
+		           }
+		           catch(NumberFormatException e) {
+		        	   System.out.println("\nnumero non valido");
+		           }
+		        }
+		        valoreValido=false;
+				if(a>10){
 					while(!valoreValido){
-					   System.out.println("\ninserire la parte reale");
+					   System.out.println("\ninserisci il valore reale");
 					   try{
 						   String numeroLetto=tastiera.readLine();
-						   cn.setRe(Double.valueOf(numeroLetto).doubleValue());
+						   coordinate.setRe(Double.valueOf(numeroLetto).doubleValue());
 						   valoreValido=true;
 					   }
-					   catch(NumberFormatException e){
-					      System.out.println("\nnumero non corretto");
-					   }
-					   catch(IllegalArgumentException e){
-						   System.out.println("\nil valore impostato non è ammesso");
+					   catch(NumberFormatException e) {
+						   System.out.println("\nnumero non valido");
+						   
 					   }
 					}
-					System.out.println("\nla parte immaginaria è "+cn.calcolaIm());
-					System.out.println("\nl'argomento è "+cn.calcolaArgument());
-				}
-				else{
 					valoreValido=false;
 					while(!valoreValido){
-				   	System.out.println("\ninserisci la parte immaginaria");
-					try{
-						String numeroLetto=tastiera.readLine();
-						cn.setIm(Double.valueOf(numeroLetto).doubleValue());
-						valoreValido=true;
+					   System.out.println("\ninserisci il valore immaginario");
+					   try{
+						   String numeroLetto=tastiera.readLine();
+						   coordinate.setIm(Double.valueOf(numeroLetto).doubleValue());
+						   valoreValido=true;
+					   }
+					   catch(NumberFormatException e) {
+						   System.out.println("\nnumero non valido");
+					   }
 					}
-					catch(NumberFormatException e){
-						System.out.println("\nnumero non corretto");
+		        	System.out.println("\nil modulo è "+coordinate.getModulus());
+		        	System.out.println("\nl'argomento è "+coordinate.getArgument());
+					
+		        }
+				else{
+					   valoreValido=false;
+						while(!valoreValido){
+						   System.out.println("\ninserisci il modulo");
+						   try{
+							   String numeroLetto=tastiera.readLine();
+							   coordinate.setModulus(Double.valueOf(numeroLetto).doubleValue());
+							   valoreValido=true;
+						   }
+						   catch(NumberFormatException e) {
+							   System.out.println("\nnumero non corretto");
+						   }
+						   catch(IllegalArgumentException e){
+							   System.out.println("\nil valore impostato non è ammesso");
+						   }
+						}
+						   valoreValido=false;
+						   while(!valoreValido){
+						      System.out.println("\ndigitare un numero minore o uguale a 10 per inserire il numero reale o uno superiore per inserire il numero immaginario");
+						      try{
+							      String numeroLetto=tastiera.readLine();
+							      a=Integer.valueOf(numeroLetto).intValue();
+							      valoreValido=true;
+						      }
+						      catch(NumberFormatException e){
+						    	  System.out.println("\nnumero non valido");
+						      }
+						}
+						if(a==1){
+							valoreValido=false;
+							while(!valoreValido){
+							   System.out.println("\ninserire la parte reale");
+							   try{
+								   String numeroLetto=tastiera.readLine();
+								   coordinate.setRe(Double.valueOf(numeroLetto).doubleValue());
+								   valoreValido=true;
+							   }
+							   catch(NumberFormatException e){
+							      System.out.println("\nnumero non corretto");
+							   }
+							   catch(IllegalArgumentException e){
+								   System.out.println("\nil valore impostato non è ammesso");
+							   }
+							}
+							System.out.println("\nla parte immaginaria è "+coordinate.getIm());
+							System.out.println("\nl'argomento è "+coordinate.getArgument());
+						}
+						else{
+							valoreValido=false;
+							while(!valoreValido){
+						   	System.out.println("\ninserisci la parte immaginaria");
+							try{
+								String numeroLetto=tastiera.readLine();
+								coordinate.setIm(Double.valueOf(numeroLetto).doubleValue());
+								valoreValido=true;
+							}
+							catch(NumberFormatException e){
+								System.out.println("\nnumero non corretto");
+							}
+							catch(IllegalArgumentException e){
+								System.out.println("\nil valore impostato non e ammesso");
+							}
+							}
+							System.out.println("\nla parte reale è "+coordinate.getRe());
+							System.out.println("\nl'argomento è "+coordinate.getArgument());
+		                                        
+						}
 					}
-					catch(IllegalArgumentException e){
-						System.out.println("\nil valore impostato non e ammesso");
-					}
-					System.out.println("\nla parte reale è "+cn.caloclaRe());
-					System.out.println("\nl'argomento è "+cn.calcolaArgument());
-				}
-			}
-		}
 	}
 
 }
